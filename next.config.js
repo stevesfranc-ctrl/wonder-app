af469f7b6ae10a824e68f294edf0bc7827a58e6d
francsteves31 <francsteves21@gmail.com>
2026-01-20 16:14:38 +0100

    fix work flow yaml

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
 const repo = "wonder-app";
+
 const nextConfig = {
   output: "export",
   basePath: `/${repo}`,
   assetPrefix: `/${repo}/`,
   images: { unoptimized: true },
 };
+
 module.exports = nextConfig;
