cba60dbcd9930b8fb00dc16785ae634091250fc6
francsteves31 <francsteves21@gmail.com>
2026-01-20 15:38:11 +0100

    Add GitHub Actions CI/CD for Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const repo = "wonder-app";
+const nextConfig = {
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
