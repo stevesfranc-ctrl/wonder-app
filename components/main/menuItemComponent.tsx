39e83e6cbe737c21777d460056c1b82c55d61d08
francsteves31 <francsteves21@gmail.com>
2026-02-08 19:35:19 +0100

    update nav bar

--- components/main/menuItemComponent.tsx
+++ components/main/menuItemComponent.tsx
@@ -19,10 +19,8 @@ export default function Menu() {
 
             <div className="w-full flex items-center justify-between backdrop-blur-md p-4">
                             {isMobileMenuOpen ?
-
-                    <div onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="w-[25px] flex items-center justify-center">
-                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" fill="#ffffff"></path></g></svg>
-                    </div>
+                            
+                    <svg onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="w-[25px] flex items-center justify-center" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" fill="#ffffff"></path></g></svg>
 
                     :
 
@@ -36,8 +34,8 @@ export default function Menu() {
                     </div>
                 }
 
-                <div onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="w-[25px] flex items-center justify-center">
-                    <svg viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+      <svg onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="w-[25px] h-[25px] flex items-center justify-center " viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                         <g id="SVGRepo_iconCarrier">
@@ -48,7 +46,7 @@ export default function Menu() {
                         </g>
                     </svg>
 
-                </div>
+        
             </div>
  
     
